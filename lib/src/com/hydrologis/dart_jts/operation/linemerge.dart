ff9b0f4de17527d4f2c750438d7822f6062a92a4
Nic Fischer <nic.r.fischer@gmail.com>
2022-08-18 18:13:57 -0700

    Start on line merger

--- lib/src/com/hydrologis/dart_jts/operation/linemerge.dart
+++ lib/src/com/hydrologis/dart_jts/operation/linemerge.dart
@@ -607,3 +607,92 @@ class DirectedEdgeEntryItem extends LinkedListEntry<DirectedEdgeEntryItem> {
     return '$edge';
   }
 }
+
+/**
+ * Merges a collection of linear components to form maximal-length linestrings. 
+ * <p> 
+ * Merging stops at nodes of degree 1 or degree 3 or more.
+ * In other words, all nodes of degree 2 are merged together. 
+ * The exception is in the case of an isolated loop, which only has degree-2 nodes.
+ * In this case one of the nodes is chosen as a starting point.
+ * <p> 
+ * The direction of each
+ * merged LineString will be that of the majority of the LineStrings from which it
+ * was derived.
+ * <p>
+ * Any dimension of Geometry is handled - the constituent linework is extracted to 
+ * form the edges. The edges must be correctly noded; that is, they must only meet
+ * at their endpoints.  The LineMerger will accept non-noded input
+ * but will not merge non-noded edges.
+ * <p>
+ * Input lines which are empty or contain only a single unique coordinate are not included
+ * in the merging.
+ *
+ * @version 1.7
+ */
+class LineMerger {
+  LineMergeGraph graph = LineMergeGraph();
+  List<LineString>? mergedLineStrings;
+  GeometryFactory? factory;
+  List edgeStrings = [];
+
+  /**
+   * Creates a new line merger.
+   *
+   */
+  LineMerger() {}
+
+  /**
+   * Adds a Geometry to be processed. May be called multiple times.
+   * Any dimension of Geometry may be added; the constituent linework will be
+   * extracted.
+   * 
+   * @param geometry geometry to be line-merged
+   */
+  void addGeometry(Geometry geometry) {
+    List lines = LinearComponentExtracter.getLines(geometry);
+    for (var line in lines) {
+      addLine(line);
+    }
+  }
+
+  /**
+   * Adds a collection of Geometries to be processed. May be called multiple times.
+   * Any dimension of Geometry may be added; the constituent linework will be
+   * extracted.
+   * 
+   * @param geometries the geometries to be line-merged
+   */
+  void addGeometries(List<Geometry> geometries) {
+    mergedLineStrings = null;
+    for (Iterator i = geometries.iterator; i.moveNext();) {
+      Geometry geometry = i.current;
+      addGeometry(geometry);
+    }
+  }
+
+  void addLine(LineString lineString) {
+    if (factory == null) {
+      this.factory = lineString.getFactory();
+    }
+    graph.addEdgefromLineString(lineString);
+  }
+
+  void merge() {
+    if (mergedLineStrings != null) {
+      return;
+    }
+
+    // reset marks (this allows incremental processing)
+    GraphComponent.setMarkedIter(graph.nodeIterator(), false);
+    GraphComponent.setMarkedIter(graph.edgeIterator(), false);
+
+    buildEdgeStringsForObviousStartNodes();
+    buildEdgeStringsForIsolatedLoops();
+    mergedLineStrings = [];
+    for (Iterator i = edgeStrings.iterator; i.moveNext();) {
+      EdgeString edgeString = i.current;
+      mergedLineStrings!.add(edgeString.toLineString());
+    }
+  }
+}
